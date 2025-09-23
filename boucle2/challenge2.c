fb319ca3dac467b7cf205f0070b621532c813c9b
aitbhou2003 <aitbhou.abdellah@gmail.com>
2025-09-23 12:38:18 +0100

    pyramide d'étoile

--- boucle2/challenge2.c
+++ boucle2/challenge2.c
@@ -3,14 +3,14 @@
 
 int main(){
     int l, i, j ;
-    printf("saisir un entier :");
+    printf("saisir un entier :"); 
     scanf("%d",&l);
 
     for (i = 1 ; i<l+1;i++){
         for(j=1;j<l-1;j++){
             printf(" ");
         }
-        
+
         for (j = 1; i < 2*1-1; j++)
         {
             printf("*");
